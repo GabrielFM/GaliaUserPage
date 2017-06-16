6457c5fcaa4078f4c6dc876871d1fcc834c95219
GabrielFM <gfm@cin.ufpe.br>
2017-06-16 10:11:19 -0300

    qualquer coisa

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 # get 'info'
 
-	post '/infoUpdate/:info/:val', to 'application#infoUpdate'
+	post '/infoUpdate/:info/:val', to: 'application#infoUpdate'
 
 end
